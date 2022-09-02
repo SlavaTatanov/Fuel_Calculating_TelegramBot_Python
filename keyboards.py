61338c06f23fe16fcb74f6342c481255f0f7048c
Slava Tatanov <slavatatanov@gmail.com>
2022-09-02 23:29:59 +0500

    another keyboard

--- keyboards.py
+++ keyboards.py
@@ -12,3 +12,14 @@ def keyboard_init():
     button = types.KeyboardButton('Fuel calculating')
     init_k.add(button)
     keyboards['keyboard_init'] = init_k
+
+
+def keyboard_people():
+    people_k = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
+    button1 = types.KeyboardButton("I'm going alone")
+    button2 = types.KeyboardButton('2')
+    button3 = types.KeyboardButton('3')
+    button4 = types.KeyboardButton('4')
+    people_k.add(button1)
+    people_k.add(button2, button3, button4)
+    keyboards['keyboard_people'] = people_k
